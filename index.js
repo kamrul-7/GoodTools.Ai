518c80cd8006a627251177b46b38bf62df1ce46e
saifulislamr16 <saifulislamr16@gmail.com>
2023-10-20 02:29:57 +0600

    branch initialize

--- index.js
+++ index.js
@@ -24,11 +24,11 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-const userCollection = client.db("goodtools").collection("tools");
+    const userCollection = client.db("goodtools").collection("tools");
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    
-    
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
