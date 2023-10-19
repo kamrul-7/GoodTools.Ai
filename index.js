606fbe213f83186842e95866cfb2e646c7813da4
sajibkhansk <sajibkhan117593@gmial.com>
2023-10-20 02:27:00 +0600

    post fix

--- index.js
+++ index.js
@@ -22,15 +22,41 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    const userCollection = client.db("goodtools").collection("tools");
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-const userCollection = client.db("goodtools").collection("tools");
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     
-    
-    // Send a ping to confirm a successful connection
+    const categoryCollection = client.db("goodtools").collection ("category");
+    const subcategoryCollection = client.db("goodtools").collection ("subcategory");
+    // Category Post
+
+    app.post("/category", async (req, res) => {
+      const item = req.body;
+      const result = await categoryCollection.insertOne(item);
+      res.send(result);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
