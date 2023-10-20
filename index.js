076426eb04e6c0f3a2a102ca20bce83bbf4d99be
saifulislamr16 <saifulislamr16@gmail.com>
2023-10-21 03:42:16 +0600

    nothing

--- index.js
+++ index.js
@@ -24,9 +24,6 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    
-    const categoryCollection = client.db("goodtools").collection ("category");
-    const subcategoryCollection = client.db("goodtools").collection ("subcategory");
 
     const categoryCollection = client.db("goodtools").collection("category");
     const usersCollection = client.db("goodtools").collection("users");
