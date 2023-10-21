1171d48b2f3d660d2ba03e107cfbcf326cd83b36
kamrul-7 <kamrul7.khan@gmail.com>
2023-10-22 00:36:31 +0600

    test

--- index.js
+++ index.js
@@ -5,11 +5,26 @@ const { ObjectId } = require('mongodb');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = 3000
 
+
+const multer = require('multer')
 app.use(cors());
 app.use(express.json());
+app.use('/uploads/', express.static('uploads'))
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname)
+  }
+})
+const upload = multer({ storage: storage })
+console.log()
 
 const uri = "mongodb+srv://goodtoolsai:aitoolsgood@cluster0.jjqth1v.mongodb.net/?retryWrites=true&w=majority";
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -20,48 +35,155 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    
+
     const categoryCollection = client.db("goodtools").collection("category");
     const usersCollection = client.db("goodtools").collection("users");
     const subcategoryCollection = client.db("goodtools").collection("subcategory");
+    const toolsCollection = client.db("goodtools").collection("tools");
+
+    // Category Post
 
     app.post("/category", async (req, res) => {
       const item = req.body;
       const result = await categoryCollection.insertOne(item);
       res.send(result);
     });
-    
+
     app.post("/users", async (req, res) => {
       const item = req.body;
       const result = await usersCollection.insertOne(item);
       res.send(result);
     });
 
+    // SunCategory Post
     app.post("/subcategory", async (req, res) => {
       const item = req.body;
       const result = await subcategoryCollection.insertOne(item);
+      console.log(result);
       res.send(result);
     });
 
-    app.get('/category', async (req, res) => {
-      const result = await categoryCollection.find().toArray();
-      res.send(result);
+    // Post a new Tool 
+    app.post("/newtool", upload.single('image'), async (req, res) => {
+      console.log(req.body);
+      const subs = req.body.SubCategory.split(',');
+      req.body.SubCategory = subs;
+      let parentCategory = []
+      if (subs) {
+        Promise.all(subs.map(async (value, index) => {
+          // Getting all the categories for the subcategories tagged for each new tool
+          const result = await subcategoryCollection.find({ SubCategory: value }).toArray()
+          if (result.length > 0 && !parentCategory.includes(result[0].category)) {
+            parentCategory.push(result[0].category)
+          }
+        }))
+          .then(async () => {
+            const data = { ...req.body, image: req.file ? req.file.path.replace(/uploads\\/g, '') : '', parentCategories : parentCategory }
+            const result = await toolsCollection.insertOne(data);
+            console.log(result);
+            res.send(result)
+          })
+      }
+
     });
+
+    // All gets starts from here
+
+    app.get('/test', async (req,res)=>{
+      const result = await toolsCollection.aggregate([
+        {
+          $unwind: "$parentCategories" // Unwind the parentCategories array to create one document per category
+        },
+        {
+          $group: {
+            _id: "$parentCategories", // Group by category
+            count: { $sum: 1 }   // Count the number of documents in each group
+          }
+        }
+      ]).toArray();
+      console.log(result);
+    })
+
+    // Get all categories
+    app.get('/category', async (req, res) => {
+      const categories = await categoryCollection.find().toArray();
+      let totalToolsCount = [];
+
+      // Get the nuber of tool for all category
+       totalToolsCount = await toolsCollection.aggregate([
+        {
+          $unwind: "$parentCategories" // Unwind the parentCategories array to create one document per category
+        },
+        {
+          $group: {
+            _id: "$parentCategories", // Group by category
+            count: { $sum: 1 }   // Count the number of documents in each group
+          }
+        }
+      ]).toArray()
+
+      // Find the number of subcategories and tools for each category
+      await Promise.all(categories.map(async (data) => {
+
+        // Find the nuber of subcategories for each category
+        const pipelineSub = [
+          {
+            $match: {
+              category: data.Title
+            }
+          },
+          {
+            $count: "count"
+          }
+        ];
+        const subCategoriesCount = await subcategoryCollection.aggregate(pipelineSub).toArray();
+
+        let c = 0;
+        if (subCategoriesCount.length > 0) {
+          c = subCategoriesCount[0].count;
+        }
+
+        // Find the number of tools for each category
+        const stat = totalToolsCount.find(category => category._id === data.Title);
+        let ct = 0;
+        if(stat){
+          ct = stat.count
+        }
+
+        // add the result and return the result
+        return { ...data, subCount: c, toolsCount: ct };
+      }))
+      .then(data => {
+          const results = [...data]
+          console.log(results);
+          res.send(results);
+      })
+    });
+
 
     app.get('/users', async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
 
+    app.get('/subcategory', async (req, res) => {
+      const result = await subcategoryCollection.find().toArray();
+      console.log(result);
+      res.send(result);
+    });
+
     app.delete('/users/:id', async (req, res) => {
       const userId = req.params.id;
+
       try {
         if (!ObjectId.isValid(userId)) {
           return res.status(400).json({ error: 'Invalid user ID' });
         }
 
         const result = await usersCollection.deleteOne({ _id: ObjectId(userId) });
+
         if (result.deletedCount === 1) {
           return res.json({ message: 'User deleted successfully' });
         } else {
@@ -74,10 +196,6 @@ async function run() {
     });
 
 
-    // Update user endpoint
-    app.put('/users/:id', async (req, res) => {
-      const userId = req.params.id;
-      const updateData = req.body;
 
 
 
@@ -95,39 +213,16 @@ async function run() {
 
 
 
-
-
-      
-      try {
-        if (!ObjectId.isValid(userId)) {
-          return res.status(400).json({ error: 'Invalid user ID' });
-        }
-
-        const updateResult = await usersCollection.updateOne(
-          { _id: ObjectId(userId) },
-          { $set: updateData }
-        );
-
-        if (updateResult.modifiedCount === 1) {
-          return res.json({ message: 'User updated successfully' });
-        } else {
-          return res.status(404).json({ error: 'User not found or no update made' });
-        }
-      } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: 'Internal server error' });
-      }
-    });
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Do not close the client connection here. It should be kept alive during the server's lifecycle.
+    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
-
 run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
